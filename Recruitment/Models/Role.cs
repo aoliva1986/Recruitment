70c6ebaef5f2f13e02dcad141a63ddbb05234268
aoliva20 <aoliva@casbelsa.com>
2026-05-13 22:41:34 -0600

    First commit

---
+++ Recruitment/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recruitment.Models
+{
+    public class Role
+    {
+        [Key]
+        public int role_id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string description { get; set; }
+    }
+}
